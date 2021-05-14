3f19a5c25dcb05400c510a63440f73ca0b213ff8
yongchen <cy19950906>
2021-05-14 15:35:59 +0800

    Brave install

---
+++ brave-auth/src/main/java/com/cloud/auth/BraveAuthApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraveAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BraveAuthApplication.class, args);
+    }
+
+}
